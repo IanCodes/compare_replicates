0770e27c84413aac7e307ce16f0a4b90ac3e6142
Ian Donaldson <IanCodes@users.noreply.github.com>
2020-06-02 12:08:19 +0100

    detect whether tools are available

--- BAM/run_rep_compare_BAM.sh
+++ BAM/run_rep_compare_BAM.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# This script requires that Deeptools is installed in your current environment, either directly or via conda, etc.
 
-cat ids.txt | parallel echo cutadapt -l 20 {}_1.fastq -o {}_1.trimmed.fq
+# Check whether Deeptools2 is present
+# method from https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+
+command -v multiBamSummary >/dev/null 2>&1 || { echo >&2 "I require multiBamSummary but it's not installed.  Aborting."; exit 1; }
+
+
+# Read in a file of input file names
+
+
+# cat ids.txt | parallel echo cutadapt -l 20 {}_1.fastq -o {}_1.trimmed.fq
