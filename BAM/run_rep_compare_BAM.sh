efc48ca2e2f13d334fb7ea066d4e1413b7ca315b
Ian Donaldson <IanCodes@users.noreply.github.com>
2020-06-03 18:02:53 +0100

    changed processor to max/2, and added Fingerprint step

--- BAM/run_rep_compare_BAM.sh
+++ BAM/run_rep_compare_BAM.sh
@@ -29,31 +29,41 @@ do
 done
 
 infiles=$(cat $files | tr '\n' ' ')
-echo $infiles
+echo INPUT files: $infiles
 
 
 #### Read in a file of input file labels
 # short descriptive name of sample to be used with '--labels' flag
 
 inlabels=$(cat $labels | tr '\n' ' ')
-echo $inlabels
-
+echo INPUT labels: $inlabels
+echo ""
 
 #### Run multiBamSummary
 echo --- Running multiBamSummary ---
-echo multiBamSummary bins --numberOfProcessors 8 --bamfiles $infiles --labels $inlabels --extendReads --ignoreDuplicates --samFlagInclude 2 --maxFragmentLength 500 -o ${name}_summary.npz
+echo multiBamSummary bins --numberOfProcessors "max/2" --bamfiles $infiles --labels $inlabels --extendReads --ignoreDuplicates --samFlagInclude 2 --maxFragmentLength 500 -o ${name}_summary.npz
+echo ""
 
 $(multiBamSummary bins --bamfiles $infiles --labels $inlabels --extendReads --ignoreDuplicates --samFlagInclude 2 --maxFragmentLength 500 -o ${name}_summary.npz) 
 
+#### Draw correlation plot
 echo --- Drawing correlation plot ---
 echo plotCorrelation -in ${name}_summary.npz --corMethod spearman --skipZeros --plotTitle "Spearman Correlation of Read Counts" --whatToPlot heatmap --colorMap RdYlBu --plotNumbers -o ${name}_heatmap_SpearmanCorr_readCounts.png --outFileCorMatrix ${name}_SpearmanCorr_readCounts.tab
+echo ""
 
 $(plotCorrelation -in ${name}_summary.npz --corMethod spearman --skipZeros --plotTitle "Spearman Correlation of Read Counts" --whatToPlot heatmap --colorMap RdYlBu --plotNumbers -o ${name}_heatmap_SpearmanCorr_readCounts.png --outFileCorMatrix ${name}_SpearmanCorr_readCounts.tab)
 
+#### Draw PCA plot
 echo --- Drawing PCA plot ---
 echo plotPCA --corData ${name}_summary.npz --plotTitle "PCA of Read Counts" -o ${name}_PCA_readCounts.png --outFileNameData ${name}_PCA_readCounts.tab
+echo ""
 
 $(plotPCA --corData ${name}_summary.npz --plotTitle "PCA of Read Counts" -o ${name}_PCA_readCounts.png --outFileNameData ${name}_PCA_readCounts.tab)
+
+#### Run 'fingerprint' analysis
+echo --- Running Fingerprint ---
+echo plotFingerprint --numberOfProcessors "max/2" --bamfiles $infiles --labels $inlabels --extendReads --ignoreDuplicates --samFlagInclude 2 --maxFragmentLength 500 -o ${name}_fingerprint.png --plotTitle="Fingerprint of Read Counts" --plotFileFormat="png"
+echo ""
 
 echo "Finished!"
 
